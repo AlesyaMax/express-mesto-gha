f430e8ed387a47054ba5586cab299eb9af3d6bc2
Alesya Maximenko <a-lesya.net@mail.ru>
2024-01-09 03:11:22 +0300

    add error management

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const { login, createUser } = require("./controllers/users");
-const auth = require("./middlewares/auth");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.post("/signin", login);
-app.post("/signup", createUser);
+app.post('/signin', login);
+app.post('/signup', createUser);
 
-app.use("/cards", auth, require("./routes/cards"));
-app.use("/users", auth, require("./routes/users"));
+app.use('/cards', auth, require('./routes/cards'));
+app.use('/users', auth, require('./routes/users'));
 
-app.use("/", (req, res) => {
-  res.status(404).send({ message: "Страница не найдена" });
+app.use('/', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
+app.use((err, req, res) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+  });
 });
 
 app.listen(3000);
--- controllers/users.js
+++ controllers/users.js
@@ -1,45 +1,43 @@
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
-const NotFoundError = require("../utils/NotFoundError");
-const AuthError = require("../utils/AuthError");
-const { generateToken } = require("../utils/jwt");
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const NotFoundError = require('../utils/NotFoundError');
+const AuthError = require('../utils/AuthError');
+const { generateToken } = require('../utils/jwt');
 
 const MONGO_DUPLICATE_ERROR_CODE = 11000;
 const SALT_ROUNDS = 10;
 
-module.exports.getUsers = async (req, res) => {
+module.exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.find({});
     return res.send(users);
   } catch (error) {
-    return res.status(500).send({ message: "На сервере произошла ошибка" });
+    next(error);
   }
 };
 
-module.exports.getUserById = async (req, res) => {
+module.exports.getUserById = async (req, res, next) => {
   try {
     const { userId } = req.params;
     const user = await User.findById(userId).orFail(
-      () => new NotFoundError("Пользователь с указанным _id не найден")
+      () => new NotFoundError('Пользователь с указанным _id не найден'),
     );
     return res.send(user);
   } catch (error) {
-    switch (error.name) {
-      case "CastError":
-        return res
-          .status(400)
-          .send({ message: "Переданы некорректные данные пользователя" });
-      case "NotFoundError":
-        return res.status(error.statusCode).send({ message: error.message });
-      default:
-        return res.status(500).send({ message: "На сервере произошла ошибка" });
+    if (error.name === 'CastError') {
+      return res
+        .status(400)
+        .send({ message: 'Переданы некорректные данные пользователя' });
     }
+    next(error);
   }
 };
 
-module.exports.createUser = async (req, res) => {
+module.exports.createUser = async (req, res, next) => {
   try {
-    const { name, about, avatar, email, password } = req.body;
+    const {
+      name, about, avatar, email, password,
+    } = req.body;
     const hash = await bcrypt.hash(password, SALT_ROUNDS);
     const newUser = await User.create({
       name,
@@ -58,21 +56,19 @@ module.exports.createUser = async (req, res) => {
   } catch (error) {
     if (error.code === MONGO_DUPLICATE_ERROR_CODE) {
       return res
-        .status(400)
-        .send({ message: "Такой пользователь уже существует" });
+        .status(409)
+        .send({ message: 'Такой пользователь уже существует' });
     }
-    switch (error.name) {
-      case "ValidationError":
-        return res.status(400).send({
-          message: error.message,
-        });
-      default:
-        return res.status(500).send({ message: error.message });
+    if (error.name === 'ValidationError') {
+      return res.status(400).send({
+        message: error.message,
+      });
     }
+    next(error);
   }
 };
 
-module.exports.editUserInfo = async (req, res) => {
+module.exports.editUserInfo = async (req, res, next) => {
   try {
     const { name, about } = req.body;
     const updatedUser = await User.findByIdAndUpdate(
@@ -81,28 +77,25 @@ module.exports.editUserInfo = async (req, res) => {
         name,
         about,
       },
-      { new: true, runValidators: true }
-    ).orFail(() => new NotFoundError("Пользователь с указанным _id не найден"));
+      { new: true, runValidators: true },
+    ).orFail(() => new NotFoundError('Пользователь с указанным _id не найден'));
     return res.send(updatedUser);
   } catch (error) {
-    switch (error.name) {
-      case "CastError":
-        return res
-          .status(400)
-          .send({ message: "Переданы некорректные данные пользователя" });
-      case "ValidationError":
-        return res.status(400).send({
-          message: "Переданы некорректные данные при обновлении профиля",
-        });
-      case "NotFoundError":
-        return res.status(error.statusCode).send({ message: error.message });
-      default:
-        return res.status(500).send({ message: "На сервере произошла ошибка" });
+    if (error.name === 'CastError') {
+      return res
+        .status(400)
+        .send({ message: 'Переданы некорректные данные пользователя' });
     }
+    if (error.name === 'ValidationError') {
+      return res.status(400).send({
+        message: 'Переданы некорректные данные при обновлении профиля',
+      });
+    }
+    next(error);
   }
 };
 
-module.exports.editAvatar = async (req, res) => {
+module.exports.editAvatar = async (req, res, next) => {
   try {
     const { avatar } = req.body;
     const updatedUser = await User.findByIdAndUpdate(
@@ -110,88 +103,72 @@ module.exports.editAvatar = async (req, res) => {
       {
         avatar,
       },
-      { new: true, runValidators: true }
-    ).orFail(() => new NotFoundError("Пользователь с указанным _id не найден"));
+      { new: true, runValidators: true },
+    ).orFail(() => new NotFoundError('Пользователь с указанным _id не найден'));
     return res.send(updatedUser);
   } catch (error) {
-    switch (error.name) {
-      case "CastError":
-        return res
-          .status(400)
-          .send({ message: "Переданы некорректные данные пользователя" });
-      case "ValidationError":
-        return res.status(400).send({
-          message: "Переданы некорректные данные при обновлении аватара",
-        });
-      case "NotFoundError":
-        return res.status(error.statusCode).send({ message: error.message });
-      default:
-        return res.status(500).send({ message: "На сервере произошла ошибка" });
+    if (error.name === 'CastError') {
+      return res
+        .status(400)
+        .send({ message: 'Переданы некорректные данные пользователя' });
     }
+    if (error.name === 'ValidationError') {
+      return res.status(400).send({
+        message: 'Переданы некорректные данные при обновлении аватара',
+      });
+    }
+    next(error);
   }
 };
 
-module.exports.login = async (req, res) => {
+module.exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email })
-      .select("+password")
-      .orFail(() => new AuthError("Неправильные почта или пароль"));
+      .select('+password')
+      .orFail(() => new AuthError('Неправильные почта или пароль'));
     const matched = await bcrypt.compare(password, user.password);
     if (!matched) {
-      throw new AuthError("Неправильные почта или пароль");
+      throw new AuthError('Неправильные почта или пароль');
     }
     const token = generateToken({ email: user.email, _id: user._id });
     return res
       .status(200)
-      .cookie("jwt", token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
+      .cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
       .send({ data: { email: user.email, _id: user._id }, token });
   } catch (error) {
-    switch (error.name) {
-      case "AuthError":
-        return res.status(error.statusCode).send({ message: error.message });
-      default:
-        return res.status(500).send({ message: "На сервере произошла ошибка" });
-    }
+    next(error);
   }
 };
 
-module.exports.getUser = async (req, res) => {
+module.exports.getUser = async (req, res, next) => {
   try {
-    const email = req.user.email;
+    const { email } = req.user;
     const user = await User.findOne({ email }).orFail(
-      () => new NotFoundError("Пользователь не найден")
+      () => new NotFoundError('Пользователь не найден'),
     );
     return res.status(200).send({ user });
   } catch (error) {
-    switch (error.name) {
-      case "NotFoundError":
-        return res.status(error.statusCode).send({ message: error.message });
-      default:
-        return res.status(500).send({ message: error.message });
-    }
+    next(error);
   }
 };
 
-module.exports.deleteUserById = async (req, res) => {
-  try {
-    const userToDelete = await User.findByIdAndDelete(req.params.userId).orFail(
-      () =>
-        new NotFoundError({ message: "Пользователь с указанным _id не найден" })
-    );
-    return res.send({
-      message: `Пользователь ${userToDelete._id}успешно удален`,
-    });
-  } catch (error) {
-    switch (error.name) {
-      case "CastError":
-        return res.status(400).send({
-          message: "Переданы некорректные данные для удаления пользователя",
-        });
-      case "NotFoundError":
-        return res.status(error.statusCode).send(error.message);
-      default:
-        return res.status(500).send({ message: "На сервере произошла ошибка" });
-    }
-  }
-};
+// Ниже функция удаления пользователя для доп. проверок, не требуется в проектной работе
+
+// module.exports.deleteUserById = async (req, res, next) => {
+//   try {
+//     const userToDelete = await User.findByIdAndDelete(req.params.userId).orFail(
+//       () => new NotFoundError({ message: 'Пользователь с указанным _id не найден' }),
+//     );
+//     return res.send({
+//       message: `Пользователь ${userToDelete._id}успешно удален`,
+//     });
+//   } catch (error) {
+//     if (error.name === 'CastError') {
+//       return res.status(400).send({
+//         message: 'Переданы некорректные данные для удаления пользователя',
+//       });
+//     }
+//     next(error);
+//   }
+// };
